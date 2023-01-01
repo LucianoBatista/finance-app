b4cfc074e67c9ace1a2b9a3dbb76778a9b8ecd1c
Luba <luciano.eq@outlook.com>
2023-01-01 15:21:05 -0300

    :sparkles: adding feature of parcelamento.

--- app/callbacks/callbacks.py
+++ app/callbacks/callbacks.py
@@ -3,19 +3,40 @@ from database.database import insert_facada
 
 
 def callback_clear_session():
-    payload = {
-        "product": st.session_state["description"],
-        "category": st.session_state["category_expense"],
-        "sub_category": st.session_state["subcategory_expense"],
-        "total": st.session_state["total"],
-        "type_spent": st.session_state["type_spent"],
-        "type_buy": st.session_state["type_buy"],
-        "bank": st.session_state["bank"],
-        "created_at": st.session_state["date_buy"].strftime("%d-%m-%Y"),
-        "due_date": st.session_state["due_date"].strftime("%d-%m-%Y"),
-    }
 
-    insert_facada(payload)
+    if st.session_state["qt_parcelas"] > 1:
+        for i in range(st.session_state["qt_parcelas"]):
+            qt_parcelas = st.session_state["qt_parcelas"]
+            description = st.session_state["description"] + " " + f"{i+1}/{qt_parcelas}"
+
+            payload = {
+                "product": description,
+                "category": st.session_state["category_expense"],
+                "sub_category": st.session_state["subcategory_expense"],
+                "total": st.session_state["total"],
+                "type_spent": st.session_state["type_spent"],
+                "type_buy": st.session_state["type_buy"],
+                "bank": st.session_state["bank"],
+                "created_at": st.session_state[f"date_buy_{i}"].strftime("%d-%m-%Y"),
+                "due_date": st.session_state[f"due_date_{i}"].strftime("%d-%m-%Y"),
+            }
+
+            insert_facada(payload)
+
+    elif st.session_state["qt_parcelas"] == 1:
+        payload = {
+            "product": st.session_state["description"],
+            "category": st.session_state["category_expense"],
+            "sub_category": st.session_state["subcategory_expense"],
+            "total": st.session_state["total"],
+            "type_spent": st.session_state["type_spent"],
+            "type_buy": st.session_state["type_buy"],
+            "bank": st.session_state["bank"],
+            "created_at": st.session_state["date_buy"].strftime("%d-%m-%Y"),
+            "due_date": st.session_state["due_date"].strftime("%d-%m-%Y"),
+        }
+
+        insert_facada(payload)
 
     for key in st.session_state.keys():
         del st.session_state[key]
@@ -27,3 +48,4 @@ def callback_clear_session():
     st.session_state["type_buy"] = "débito"
     st.session_state["bank"] = "Nubank"
     st.session_state["user"] = "Tica"
+    st.session_state["parcela"] = "N"
